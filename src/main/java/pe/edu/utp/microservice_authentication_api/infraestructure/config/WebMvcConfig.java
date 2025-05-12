4e3fb876b6fbe64380813b14657d54bb3592df08
Omaly Andres Aguilar Regalado <104542376+icarius4iu@users.noreply.github.com>
2025-05-12 15:47:00 -0500

    a

---
+++ src/main/java/pe/edu/utp/microservice_authentication_api/infraestructure/config/WebMvcConfig.java
@@ -0,0 +1,10 @@
+package pe.edu.utp.microservice_authentication_api.infraestructure.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig implements WebMvcConfigurer {
+}
